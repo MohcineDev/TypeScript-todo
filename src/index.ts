1af3c078c2a7a1233483c9eae61fbdf398c3d51b
MohcineDev <mouhcinedev@gmail.com>
2022-05-03 23:54:54 +0000

    add uuid

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log(123);
+import {v4 as uuidV4} from 'uuid'
+
+const list = document.querySelector<HTMLUListElement>('#list')
+const form = document.querySelector<HTMLFormElement>('#new-task-form')
+const input = document.querySelector<HTMLInputElement>('#new-task-title')
+
+form?.addEventListener('submit', e=>{
+    e.preventDefault()
+
+    if (input?.value == "" || input?.value == null) {
+        return 
+    }
+
+    const newTask = {
+        id:uuidV4(),
+        title:input.value,
+        completed:false,
+        createdAt:new Date()
+    }
+    addListItem(newTask)
+})
+
+
+function addListItem(task:{        id:string,
+    title:string,
+    completed:boolean,
+    createdAt:Date}){
+
+}
